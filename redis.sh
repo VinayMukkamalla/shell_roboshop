46aa3690aa7a79d156d8cb3786061e231f325fb8
VinayMukkamalla <vinay.mukkamalla@gmail.com>
2025-09-30 16:20:58 +0530

    adding to repo

--- redis.sh
+++ redis.sh
@@ -41,7 +41,7 @@ dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis 7"
 
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATE $? "updating redis configuration allowing access"
 
 systemctl enable redis &>>$LOG_FILE
